1b56a641b66ee83d78f7ac6c930ee2f2fa60549c
Mykhailo Oliinyk <mykhailo.oliinyk9@gmail.com>
2026-09-22 13:37:29 +0300

    Solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,82 @@
 'use strict';
 
+class Listener {
+  constructor(func, once = false) {
+    this.func = func;
+    this.isOnce = once;
+  }
+}
+
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  events = {}; // {'error': [Listener, Listener]}
+
+  on(event, func) {
+    if (Array.isArray(this.events[event])) {
+      this.events[event].push(new Listener(func));
+    } else {
+      this.events[event] = [new Listener(func)];
+    }
+  }
+
+  once(event, func) {
+    if (Array.isArray(this.events[event])) {
+      this.events[event].push(new Listener(func, true));
+    } else {
+      this.events[event] = [new Listener(func, true)];
+    }
+  }
+
+  off(event, funcToRemove) {
+    if (Array.isArray(this.events[event])) {
+      this.events[event] = this.events[event].filter(
+        (listener) => listener.func !== funcToRemove,
+      );
+    }
+  }
+
+  emit(event, ...args) {
+    if (Array.isArray(this.events[event])) {
+      this.events[event].forEach((listener) => {
+        listener.func.call(this, ...args);
+
+        if (listener.isOnce) {
+          this.off(event, listener.func);
+        }
+      });
+    }
+  }
+
+  prependListener(event, func) {
+    if (Array.isArray(this.events[event])) {
+      this.events[event].unshift(new Listener(func));
+    } else {
+      this.events[event] = [new Listener(func)];
+    }
+  }
+
+  prependOnceListener(event, func) {
+    if (Array.isArray(this.events[event])) {
+      this.events[event].unshift(new Listener(func, true));
+    } else {
+      this.events[event] = [new Listener(func, true)];
+    }
+  }
+
+  removeAllListeners(event) {
+    if (event) {
+      this.events[event] = null;
+    } else {
+      this.events = {};
+    }
+  }
+
+  listenerCount(event) {
+    if (Array.isArray(this.events[event])) {
+      return this.events[event].length;
+    }
+
+    return 0;
+  }
 }
 
 module.exports = MyEventEmitter;
